c8a02434ccb946e7afee527494c46b130d9b1eb9
Eduard <benediceduard@outlook.com>
2021-02-13 18:46:57 +0000

    initial stage doesn't really matter

---
+++ src/medusa.ts
@@ -0,0 +1,16 @@
+import { BaseOptions } from './types/options.ts'
+
+console.log(BaseOptions)
+
+// class Medusa {
+//     baseEl: HTMLElement;
+//     constructor(selector : string, options: DefaultProps) {
+//         this.baseEl : HTMLElement = document.querySelector(selector)
+//         console.log(options)
+//     }
+// }
+
+// var med = new Medusa("#def", {
+//     placeholder: "I am a placeholder",
+//     visibleOpt: 4
+// })
---
+++ src/types/options.ts
@@ -0,0 +1,35 @@
+
+
+export interface BaseOptions {
+    /*
+    The placeholder to be displayed in the container that will trigger the
+    pop up of the entire dropdown.
+    */
+    placeholder: string;
+    
+    /* The number of comma separeted values to be displayed */
+    csv: number;
+
+    /* How you want to see the format */
+    captionFormat: string;
+
+    /* The text to be shown when all options are selected */
+    allSelected: string;
+    
+    /* If true selects all options by default */
+    selectAll: boolean;
+
+    /* Enable search */
+    search: boolean;
+
+}
+
+
+export const defaults: BaseOptions = {
+    placeholder: "Search something",
+    csv: 3,
+    captionFormat: "habar n-am",
+    allSelected: "Are all selected",
+    selectAll: false,
+    search: false
+}
